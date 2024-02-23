452d6a8e697b8ae6501bf31c768c3916d4f3bb8f
MariaFlorenciaGala <mariaflorenciagala8@gmail.com>
2024-02-23 07:34:42 -0300

    cree el camino a la card para probar que me muestre los countries

---
+++ client/src/components/details/details.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './details.css'
+
+
+export default function Details(){
+  return (
+    <div className='details'>
+        <div>Subregión {}</div>
+        <div>Área: {}</div>
+        <div>Población: {}</div>
+        <div>Ir: {}</div>
+    </div>
+  )
+}
+
